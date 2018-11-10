4dfb80bec8834aec0156deb307618e575c2b9cb0
cy-campos <cy.campos1983@gmail.com>
2018-11-10 08:20:32 -0500

    Project updated
    Initial commit of HW4
    Test sandbox class

---
+++ src/Hw4_Campos.java
@@ -0,0 +1,50 @@
+/*
+    Title:          HomeworkAssignment3
+    Scope:          CSC 200 (002M) - Yun-Sheng Wang, Ph.D
+    Author:         Christopher Yap Campos
+    Contact:        cy.campos1983@gmail.com
+    Created:        17OCT2018
+    Dependencies:   Requires java 10 in order to use inference type "var"
+    Summary:        HW2
+
+    Instructions:
+
+        HW # 4
+        Posted on: Saturday, November 3, 2018 8:04:50 AM EDT
+
+        Due: 8am 11/17/2018
+
+        PRELIMINARY SPECIFICATION:
+
+        Specify the model with 3 numbers:
+
+        > 1 3 5     (They can be 1, 7, and 19)
+
+        The bias is: xxx
+
+        Give me a 0/1 representation based on the above model:
+
+        > 010100011
+
+        The above 0/1 represents:
+
+        > yyyy
+
+        Give me a decimal:
+
+        > -81.234
+
+        Hey, it cannot be represented! Try again.
+
+        Give me a decimal:
+
+        -1.23
+
+        The above decimal is represented as:
+
+        0111011111 (you figure it out)
+*/
+
+public class Hw4_Campos {
+    public static void main(String[] args) {}
+}
--- src/Test.java
+++ src/Test.java
@@ -9,71 +9,15 @@ public class Test {
 
     public static void main(String[] args) {
 
-        // How many students?
-        System.out.println("How many students?");
-        int studentCount = _scanner.nextInt();
+        char blah = '0';
+        int blah2 = (int)blah;
 
-        // scores for student 1, 2, etc.
-        List<Student> studentList = new ArrayList<Student>();
+        System.out.println(blah2);
+        
+        byte[] byteArray = new byte[20];
 
-        for (int i = 0; i < studentCount; i++) {
-            System.out.println("Scores for student " + i + ":");
+        for (byte bit: byteArray) {
 
-            Student newStudent = new Student();
-
-            double entry = 0;
-
-            // entries
-            do {
-                entry = _scanner.nextDouble();
-
-                if (entry == -1)
-                    break;
-
-                newStudent.gradeList.add(entry);
-            } while (entry != -1);
-
-            newStudent.setAverageGrade();
-
-            studentList.add(newStudent);
-        }
-
-        Double gradeSum = new Double(0);
-        Double count = new Double(0);
-
-        for (int i = 0; i < studentList.size(); i++) {
-            Double value = studentList.get(i).averageGrade;
-            System.out.println("Student " + i + " Avg: " + value);
-
-            for (int x = 0; x < studentList.get(i).gradeList.size(); x++) {
-                gradeSum += studentList.get(i).gradeList.get(x);
-                count++;
-            }
-        }
-
-        System.out.println("\n\nClass Avg: " + gradeSum / count);
-    }
-
-
-    public static class Student {
-        Double averageGrade;
-        List<Double> gradeList;
-
-        public Student() {
-            averageGrade = new Double(0);
-            gradeList = new ArrayList<Double>();
-        }
-
-        public Double getAverageGrade() {
-            Double sum = 0.0;
-            for (int i = 0; i < gradeList.size(); i++)
-                sum += gradeList.get(i);
-
-            return sum / gradeList.size();
-        }
-
-        public void setAverageGrade() {
-            averageGrade = getAverageGrade();
         }
     }
 }
